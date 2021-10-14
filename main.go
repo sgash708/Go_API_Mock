bebcc5c00e4020a4701fd798e40ed3a9a32ba66e
sgash708 <sugash7081023@gmail.com>
2021-10-14 22:17:22 +0900

    雛形作成

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+type Student struct {
+	Nmae   string
+	Number int
+	Grade  int
+}
+type Teacher struct {
+	Name string
+}
+
+type Person interface {
+	getEmail() string
+}
+
+func main() {
+}
